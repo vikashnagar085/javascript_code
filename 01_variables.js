914738d4fc67cd6610cf7589b01b9dd58f49187c
vikashnagar085 <vikashnagar085@gmail.com>
2024-06-04 23:43:43 +0530

    Add files via upload

---
+++ 01_variables.js
@@ -0,0 +1,16 @@
+console.log("Hello")
+console.log("This is the Puppy Song");
+/*
+their is and issue in block and function level scope in var so instead of this we use 
+let and const
+*/
+const name = "Vikash Nagar";
+let num = 123456789;
+var pass = "12345";
+name1 = "vikash Gurjar"
+let name3;
+
+/*
+If we don't pass the value in variable then it will show undefined.
+*/
+console.table([name,num,pass,name1,name3])
